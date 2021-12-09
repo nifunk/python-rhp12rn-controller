966a2457772e4c8a33998c25018018dad52c2c3e
Tim Schneider <tim@robot-learning.de>
2021-12-09 13:58:17 +0100

    Added futures to module exports

--- rhp12rn/__init__.py
+++ rhp12rn/__init__.py
@@ -1,4 +1,4 @@
-from .dynamixel_connector import DynamixelConnector
+from .dynamixel_connector import DynamixelConnector, FieldReadFuture, FieldWriteFuture, DynamixelFuture
 from .rhp12rn_connector import RHP12RNConnector, RHP12RN_FIELDS, RHP12RN_RAM_FIELDS, RHP12RN_EEPROM_FIELDS
 from .rhp12rna_connector import RHP12RNAConnector, RHP12RNA_FIELDS, RHP12RNA_RAM_FIELDS, RHP12RNA_EEPROM_FIELDS
 from .rhp12rn import RHP12RN
