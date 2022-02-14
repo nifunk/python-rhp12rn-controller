84ea0909941a32fc1ba89f3198a990b1fa0a5bf9
Tim Schneider <tim@robot-learning.de>
2022-02-14 13:07:19 +0100

    Added long_description to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(
     name="rhp12rn",
     version="1.0.0",
     description="Python controller for the ROBOTIS RH-P12-RN and RH-P12-RN(A) grippers.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/TimSchneider42/python-rhp12rn-controller",
     author="Tim Schneider",
     author_email="schneider@ias.informatik.tu-darmstadt.de",
@@ -11,7 +16,7 @@ setup(
     packages=["rhp12rn"],
     install_requires=[
         "dynamixel-sdk @ git+https://github.com/ROBOTIS-GIT/DynamixelSDK.git@c7e1eb71c911b87f7bdeda3c2c9e92276c2b4627#egg=dynamixel-sdk&subdirectory=python"
-        ],
+    ],
 
     classifiers=[
         "Intended Audience :: Science/Research",
